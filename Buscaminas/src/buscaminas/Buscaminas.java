6b31c5e3b1e23ae2c7624dd801c29628faf5dc24
lmfernandezmar <37187724+lmfernandezmar@users.noreply.github.com>
2018-04-16 10:04:06 +0200

    PruebaFinal

--- Buscaminas/src/buscaminas/Buscaminas.java
+++ Buscaminas/src/buscaminas/Buscaminas.java
@@ -7,9 +7,6 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Buscaminas extends JFrame implements ActionListener, MouseListener {
     
-    /*HOLA ANDREA*/
-    //Hola Luis
-    
     int nomines = 80;
     int perm[][];
     String tmp;
